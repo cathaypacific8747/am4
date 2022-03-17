82a1a664da448a0c9b66fd812d4db68e6c4c1987
cathaypacific8747 <58929011+cathaypacific8747@users.noreply.github.com>
2022-03-17 16:50:06 +0800

    add postgres req

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # V3
 
 A modern rewrite of the discord bot.
+
+```
+$ sudo apt-get update
+$ sudo apt-get install postgresql
+$ sudo passwd postgres
+$ sudo service postgresql start
+$ sudo -u postgres psql
+postgres-# \du
+postgres-# ALTER USER postgres WITH PASSWORD 'XXX';
+```
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
     load_dotenv()
-    discord_token = os.getenv('DISCORD_TOKEN_PRODUCTION' if os.getenv('MODE') == 'PRODUCTION' else 'DISCORD_TOKEN_DEVELOPMENT')
+
+    discord_token = os.getenv('DISCORD_TOKEN')
     assert discord_token is not None
     
     print(discord_token)
